37e1d135d76143c22ca41fded496c873955a00a3
Marcin Jabłoński <jablon1994@gmail.com>
2016-03-07 20:44:24 +0100

    Patient data changes

--- eObchod.Server/eObchod.Server.Database/Entities/Patient.cs
+++ eObchod.Server/eObchod.Server.Database/Entities/Patient.cs
@@ -1,12 +1,17 @@
-﻿namespace eObchod.Server.Database.Entities
+﻿using System;
+
+namespace eObchod.Server.Database.Entities
 {
     public class Patient
     {
         public int PatientId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int Age { get; set; }
+        public int Gender { get; set; }
         public int WardId { get; set; }
         public int BlockId { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime AdmittanceDate { get; set; }
+        public DateTime DischargeDate { get; set; }
     }
 }
