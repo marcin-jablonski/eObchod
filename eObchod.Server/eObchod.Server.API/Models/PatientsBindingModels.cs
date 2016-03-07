ae027fe82298c7a79e2c86073567e40c00a52e4f
Marcin Jabłoński <jablon1994@gmail.com>
2016-03-07 20:53:34 +0100

    Removed redundant code

--- eObchod.Server/eObchod.Server.API/Models/PatientsBindingModels.cs
+++ eObchod.Server/eObchod.Server.API/Models/PatientsBindingModels.cs
@@ -5,24 +5,11 @@ namespace eObchod.Server.API.Models
 {
     public class PatientBindingModel
     {
-        public int PatientId;
-        public string FirstName;
-        public string LastName;
-        public DateTime BirthDate;
-        public int WardId;
-        public int BlockId;
+        //to fill later
 
         public static explicit operator Patient(PatientBindingModel model)
         {
-            return new Patient
-            {
-                PatientId = model.PatientId,
-                BirthDate = model.BirthDate,
-                BlockId = model.BlockId,
-                FirstName = model.FirstName,
-                LastName = model.LastName,
-                WardId = model.WardId
-            };
+            return new Patient();
         }
     }
 }
