c31959eed12b84b03ea48b700cd8f816eb2ee8b3
Marcin Jabłoński <jablon1994@gmail.com>
2016-04-04 21:58:27 +0200

    Smaple controller

---
+++ eObchod.Server/eObchod.Server.API/Controllers/SampleController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace eObchod.Server.API.Controllers
+{
+    [Route("api/Sample")]
+    public class SampleController : ApiController
+    {
+        [HttpGet]
+        public object GetSample()
+        {
+            return @"Hello world! To jest jakaś porażka!";
+        }
+    }
+}
