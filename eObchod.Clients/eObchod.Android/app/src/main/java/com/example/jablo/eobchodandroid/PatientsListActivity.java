813f02f64bc971592333ec4441b881da672a2e46
Marcin Jabłoński <jablon1994@gmail.com>
2016-04-18 14:13:10 +0200

    Activities up to patient data (still to implement)

--- eObchod.Clients/eObchod.Android/app/src/main/java/com/example/jablo/eobchodandroid/PatientsListActivity.java
+++ eObchod.Clients/eObchod.Android/app/src/main/java/com/example/jablo/eobchodandroid/PatientsListActivity.java
@@ -2,65 +2,63 @@ package com.example.jablo.eobchodandroid;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class RoomListActivity extends AppCompatActivity {
+public class PatientsListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_room_list);
+        setContentView(R.layout.activity_patients_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        LinearLayout blockList = (LinearLayout) findViewById(R.id.room_list);
-        //get room list from api
+        LinearLayout patientsList = (LinearLayout) findViewById(R.id.patients_list);
+        //get patients list from api
         //foreach room add TextView
         TextView sample1 = new TextView(this);
         sample1.setPadding(10, 10, 10, 10);
         sample1.setTextSize(30);
-        sample1.setText("Room 1");
+        sample1.setText("Patient 1");
         sample1.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        sample1.setWidth(blockList.getWidth());
+        sample1.setWidth(patientsList.getWidth());
         sample1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onRoomItemClick(v);
+                onPatientItemClick(v);
             }
         });
 
         TextView sample2 = new TextView(this);
         sample2.setPadding(10, 10, 10, 10);
         sample2.setTextSize(30);
-        sample2.setText("Room 2");
+        sample2.setText("Patient 2");
         sample2.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        sample2.setWidth(blockList.getWidth());
+        sample2.setWidth(patientsList.getWidth());
         sample2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onRoomItemClick(v);
+                onPatientItemClick(v);
             }
         });
 
-        blockList.addView(sample1);
-        blockList.addView(sample2);
+        patientsList.addView(sample1);
+        patientsList.addView(sample2);
     }
 
-    public void onRoomItemClick(View view) {
+    public void onPatientItemClick(View view) {
         //get block data and load to intent
-        startPatientsListActivity();
+        startPatientDataActivity();
     }
 
-    public void startPatientsListActivity(){
-        Intent intent = new Intent(this, PatientsListActivity.class);
+    public void startPatientDataActivity(){
+        Intent intent = new Intent(this, PatientDataActivity.class);
         startActivity(intent);
     }
 }
