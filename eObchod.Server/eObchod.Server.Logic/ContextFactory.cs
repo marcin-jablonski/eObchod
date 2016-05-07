1419325173bbc20f872ba3b270cc24c8426f3ba0
Marcin Jabłoński <jablon1994@gmail.com>
2016-05-07 15:43:12 +0200

    Factory update

--- eObchod.Server/eObchod.Server.Logic/ContextFactory.cs
+++ eObchod.Server/eObchod.Server.Logic/ContextFactory.cs
@@ -6,15 +6,22 @@ namespace eObchod.Server.Logic
     public class ContextFactory
     {
         private static IPatientContext _patientContext;
+        private static IHospitalStructureContext _hospitalStructureContext;
 
         static ContextFactory()
         {
             _patientContext = new PatientContext(new PatientsManager());
+            _hospitalStructureContext = new HospitalStructureContext(new BlockManager(), new WardManager(), new RoomManager());
         }
 
         public static IPatientContext GetPatientContext()
         {
             return _patientContext;
+        }
+
+        public static IHospitalStructureContext GetHospitalStructureContext()
+        {
+            return _hospitalStructureContext;
         }
     }
 }
