82b893de78bd88e3ab16a3ab925e34dde97efe61
Marcin Jabłoński <jablon1994@gmail.com>
2016-05-07 15:25:53 +0200

    Data Structures project added

---
+++ eObchod.Server/eObchod.Server.DataStructures/PatientListItem.cs
@@ -0,0 +1,19 @@
+﻿using eObchod.Server.Database.Entities;
+
+namespace eObchod.Server.DataStructures
+{
+    public class PatientListItem
+    {
+        public string Pesel { get; set; }
+        public string Name { get; set; }
+
+        public static explicit operator PatientListItem(Patient patient)
+        {
+            return new PatientListItem
+            {
+                Name = patient.FirstName + " " + patient.LastName,
+                Pesel = patient.Pesel
+            };
+        }
+    }
+}
